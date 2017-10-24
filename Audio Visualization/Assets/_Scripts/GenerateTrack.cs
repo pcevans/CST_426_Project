d16af0ee6f5c3102f9bb89868efb1a21526962d7
pcevans <phevans@csumb.edu>
2017-10-23 21:45:44 -0700

    the beginning of something usable

--- Audio Visualization/Assets/_Scripts/GenerateTrack.cs
+++ Audio Visualization/Assets/_Scripts/GenerateTrack.cs
@@ -7,57 +7,53 @@ public class GenerateTrack : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Vector3[] tpa = GenerateFreqAvgBandPath.trackPointArray;
-
-		float width;
-		float height;
+	
 		MeshFilter mf = GetComponent<MeshFilter> ();
 		Mesh mesh = new Mesh ();
 		mf.mesh = mesh;
 
+		/**/
 		Vector3[] vertices = new Vector3[(50 + 1) * 2];
 
-		vertices [0] = new Vector3 (-4, 0, 0);
-		vertices [1] = new Vector3 (4, 0, 0);
-		for (int i = 1; i <= 50; i++) {
-			vertices [i + 0] = new Vector3 (-4, 0, i);
-			vertices [i + 2] = new Vector3 (4, 0, i);
+		//vertices [0] = new Vector3 (-4, 0, 0);
+		//vertices [1] = new Vector3 (4, 0, 0);
+		for (int i = 0; i <= 50; i++) {
+			vertices [2 * i + 0] = new Vector3 (-4, 0, i);
+			vertices [2 * i + 1] = new Vector3 (4, 0, i);
 		}
 
 		mesh.vertices = vertices;
 
 		int[] tri = new int[50 * 6];
 
-
 		for (int i = 0; i < 50; i++) {
-			
-			tri [6 * i + 0] = 4 * i + 0;
-			tri [6 * i + 1] = 4 * i + 2;
-			tri [6 * i + 2] = 4 * i + 1;
+			tri [6 * i + 0] = 2 * i + 0;
+			tri [6 * i + 1] = 2 * i + 2;
+			tri [6 * i + 2] = 2 * i + 1;
 
-			tri [6 * i + 3] = 4 * i + 2;
-			tri [6 * i + 4] = 4 * i + 3;
-			tri [6 * i + 5] = 4 * i + 1;
+			tri [6 * i + 3] = 2 * i + 1;
+			tri [6 * i + 4] = 2 * i + 2;
+			tri [6 * i + 5] = 2 * i + 3;
 		}
 
 		mesh.triangles = tri;
 
-		Vector3[] normals = new Vector3[4];
+		Vector3[] normals = new Vector3[102];
 
-		normals [0] = -Vector3.forward;
-		normals [1] = -Vector3.forward;
-		normals [2] = -Vector3.forward;
-		normals [3] = -Vector3.forward;
+		for (int i = 0; i < 102; i++) {
+			normals [i] = -Vector3.up;
+		}
 
 		mesh.normals = normals; 
 
-		Vector2[] uv = new Vector2[4];
-
-		uv [0] = new Vector2 (0, 0);
-		uv [1] = new Vector2 (1, 0);
-		uv [2] = new Vector2 (0, 1);
-		uv [3] = new Vector2 (1, 1);
+		Vector2[] uv = new Vector2[102];
+		for (int i = 0; i < 51; i++) {
+			uv [2 * i + 0] = new Vector2 (0, i / 50f);
+			uv [2 * i + 1] = new Vector2 (1, i / 50f);
+		}
 
 		mesh.uv = uv;
+		/**/
 
 	}
 	
