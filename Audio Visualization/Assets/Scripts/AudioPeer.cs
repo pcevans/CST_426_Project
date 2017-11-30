bee8a23bac2cedea9fe88bfb0a7d7c57c1c4b151
pcevans <phevans@csumb.edu>
2017-11-30 12:09:14 -0800

    idk

---
+++ Audio Visualization/Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+
+namespace DeckRacer {
+	[RequireComponent (typeof(AudioSource))]
+	public class AudioPeer : MonoBehaviour {
+
+		AudioSource _audioSource;
+		public static float[] _samplesLeft = new float[512];
+		public static float[] _samplesRight = new float[512];
+
+		public static int _numBands = 8;
+		float[] _freqBand;
+		float[] _freqBandBuffer;
+		float[] _freqBufferDecrease;
+		float[] _freqBandHighest;
+		public static int _avgFreqBand;
+
+		public static float[] _audioBand;
+		public static float[] _audioBandBuffer;
+
+		public static float _Amplitude, _AmplitudeBuffer;
+		float _AmplitudeHighest;
+		public float _audioProfile;
+
+
+		public enum _channel {
+			Stereo,
+			Left,
+			Right
+		}
+
+		public _channel channel = new _channel ();
+
+		void Awake () {
+			_freqBand = new float[_numBands];
+			_freqBandBuffer = new float[_numBands];
+			_freqBufferDecrease = new float[_numBands];
+			_freqBandHighest = new float[_numBands];
+
+			_audioBand = new float[_numBands];
+			_audioBandBuffer = new float[_numBands];
+		}
+
+		void Start () {
+			_audioSource = GetComponent<AudioSource> ();
+			AudioProfile (_audioProfile);
+		}
+
+		void Update () {
+			GetSpectrumAudioSource ();
+			MakeFrequencyBands ();
+			BandBuffer ();
+			CreateAudioBands ();
+			GetAverageAudioBand ();
+			GetAmplitude ();
+
+		}
+
+		void AudioProfile (float audioProfile) {
+			for (int i = 0; i < _numBands; i++) {
+				_freqBandHighest [i] = audioProfile;
+			}
+		}
+
+		void GetSpectrumAudioSource () {
+			_audioSource.GetSpectrumData (_samplesLeft, 0, FFTWindow.Blackman);
+			_audioSource.GetSpectrumData (_samplesRight, 1, FFTWindow.Blackman);
+		}
+
+		void MakeFrequencyBands () {
+			int count = 0;
+			for (int i = 0; i < _numBands; i++) {
+				float average = 0;
+				int sampleCount = (int)Mathf.Pow (2, i + 1);
+				if (i == 7)
+					sampleCount += 2;
+
+				for (int j = 0; j < sampleCount; j++) {
+					average += _samplesLeft [count] + _samplesRight [count] * (count + 1);
+					count++;
+				}
+				average /= count;
+
+				_freqBand [i] = average * 10;
+			}
+		}
+
+		void BandBuffer () {
+			for (int i = 0; i < _numBands; i++) {
+				if (_freqBand [i] > _freqBandBuffer [i]) {
+					_freqBandBuffer [i] = _freqBand [i];
+					_freqBufferDecrease [i] = 0.005f;
+				}
+
+				if (_freqBand [i] < _freqBandBuffer [i]) {
+					_freqBandBuffer [i] -= _freqBufferDecrease [i];
+					if (_freqBandBuffer [i] < 0)
+						_freqBandBuffer [i] = 0;
+					_freqBufferDecrease [i] *= 1.2f;
+				}
+			}
+		}
+
+		void CreateAudioBands () {
+			for (int i = 0; i < _numBands; i++) {
+				if (_freqBand [i] > _freqBandHighest [i]) {
+					_freqBandHighest [i] = _freqBand [i];
+				}
+				_audioBand [i] = (_freqBand [i] / _freqBandHighest [i]);
+				_audioBandBuffer [i] = (_freqBandBuffer [i] / _freqBandHighest [i]);
+			}
+		}
+
+		void GetAverageAudioBand () {
+			float sum = 0;
+			float denominator = 0;
+			for (int i = 0; i < _numBands; i++) {
+				denominator += _audioBand [i];
+				sum += i * _audioBand [i];
+			}
+			sum /= denominator;
+			_avgFreqBand = (int)sum;//((int)sum + _avgFreqBand) / 2;
+		}
+
+		void GetAmplitude () {
+			float _CurrentAmplitude = 0;
+			float _CurrentAmplitudeBuffer = 0;
+
+			for (int i = 0; i < _numBands; i++) {
+				_CurrentAmplitude += _audioBand [i];
+				_CurrentAmplitudeBuffer += _audioBandBuffer [i];
+			}
+
+			if (_CurrentAmplitude > _AmplitudeHighest)
+				_AmplitudeHighest = _CurrentAmplitude;
+
+			_Amplitude = _CurrentAmplitude / _AmplitudeHighest;
+			_AmplitudeBuffer = _CurrentAmplitudeBuffer / _AmplitudeHighest;
+		}
+	}
+}
---
+++ Audio Visualization/Assets/Scripts/GenerateFreqAvgBandPath.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeckRacer {
+	[RequireComponent (typeof(LineRenderer))]
+	public class GenerateFreqAvgBandPath : MonoBehaviour {
+		public GameObject trackPrefab;
+
+		LineRenderer lr;
+		Vector3 currPoint;
+		Quaternion currRot;
+		float currHeight;
+		int frame = 0;
+		public int numberOfPoints;
+		public bool trackGenOn;
+		public static Vector3[] trackPointArray = new Vector3[500];
+
+		const float angleMax = 45;
+		float angleXZ;
+		float angleY;
+		float clampNeg;
+		float clampPos;
+
+		void Start () {
+			lr = GetComponent<LineRenderer> ();
+			lr.positionCount = 1;
+			currPoint = new Vector3 (1, 0, 0);
+			currRot = Quaternion.identity;
+			currHeight = 5f;
+			angleXZ = 0;
+			angleY = 0;
+			clampPos = 1;
+			clampNeg = -1;
+		}
+
+		void Update () {
+			if (frame / 10 <= numberOfPoints) {
+
+				currPoint.y = 0;
+				if (frame % 3 == 0) {
+					float rotVal = 0;
+					for (int i = 0; i < 8; i++) {
+						float bandVal = AudioPeer._audioBand [i];
+						float totalPercent = 1 / Mathf.Pow (2, AudioPeer._avgFreqBand - (i + 1) + 1);
+						float direction = ((AudioPeer._audioBand [i] - AudioPeer._Amplitude) > 0 ? 1 : -1);
+						rotVal += bandVal * totalPercent * direction;
+					}
+
+					if (rotVal > clampPos) rotVal = clampPos;
+					else if (rotVal < clampNeg) rotVal = clampNeg;
+
+					Debug.Log (rotVal + " " + clampPos + " " + clampNeg);
+
+					angleXZ += rotVal;
+
+					if (angleXZ > angleMax) {
+						angleXZ = angleMax;
+						clampNeg += -.1f;
+					} else if (angleXZ < -angleMax) {
+						angleXZ = -angleMax;
+						clampPos += .1f;
+					} else {
+						clampNeg = (clampNeg * 3 + -1.0f) / 4f;
+						clampPos = (clampPos * 3 + 1.0f) / 4f;
+					}
+
+
+
+					Debug.Log (angleXZ);
+
+					currRot = Quaternion.Slerp (currRot, Quaternion.AngleAxis (angleXZ, Vector3.up * 10), .7f);
+				}
+				if (frame % 5 == 0) {
+
+					angleY += .01f;
+					currHeight = Mathf.Sin (angleY) * 10;
+
+					currPoint += currRot * Vector3.Normalize (currPoint);
+					currPoint.y = currHeight;
+					lr.positionCount += 1;
+					lr.SetPosition (lr.positionCount - 1, currPoint);
+
+				}
+
+				frame++;
+			}
+		}
+	}
+}
