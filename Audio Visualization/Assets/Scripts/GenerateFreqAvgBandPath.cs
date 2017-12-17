84fcb8e310502e68abe4be13ed86e056d76a39cd
pcevans <phevans@csumb.edu>
2017-12-16 21:09:33 -0800

    add

--- Audio Visualization/Assets/Scripts/GenerateFreqAvgBandPath.cs
+++ Audio Visualization/Assets/Scripts/GenerateFreqAvgBandPath.cs
@@ -9,6 +9,7 @@ public class GenerateFreqAvgBandPath : MonoBehaviour {
 	public GameObject car;
 
 	LineRenderer lr;
+    Vector3 lastCarPos;
 	Vector3 currPoint;
 	Quaternion currRot;
 	float currHeight;
@@ -30,7 +31,8 @@ public class GenerateFreqAvgBandPath : MonoBehaviour {
 	void Start () {
 		lr = GetComponent<LineRenderer> ();
 		lr.positionCount = 1;
-		currPoint = new Vector3 (1, 0, 0);
+        lastCarPos = car.transform.position;
+		currPoint = new Vector3 (0, 0, 0);
 		currRot = Quaternion.identity;
 		currHeight = 5f;
 		angleXZ = 0;
@@ -59,88 +61,108 @@ public class GenerateFreqAvgBandPath : MonoBehaviour {
 	}
 
 	void Update () {
-		//if (lr.positionCount < numberOfPoints) {
-		currPoint.y = 0;
-		if (frame % 3 == 0) {
-			//float[] percentages = CalculatePercentages (AudioPeer._avgFreqBand, AudioPeer._numBands);
-			//Debug.Log (AudioPeer._Amplitude);
-			float max = 0;
-			int maxIdx = -1;
-			for (int i = 0; i < 8; i++) {
-				float bandVal = AudioPeer._audioBand [i];
+        float amtMoved = Vector3.Magnitude(car.transform.position - lastCarPos);
+        if (amtMoved < .5f || Vector3.Magnitude(lr.GetPosition(lr.positionCount - 1) - car.transform.position) > 60f) 
+            amtMoved = .5f;
+        //if (lr.positionCount < numberOfPoints) {
+        if (amtMoved > .000001)
+        {
+            currPoint.y = 0;
+            if (frame % 3 == 0)
+            {
+                //float[] percentages = CalculatePercentages (AudioPeer._avgFreqBand, AudioPeer._numBands);
+                //Debug.Log (AudioPeer._Amplitude);
+                float max = 0;
+                int maxIdx = -1;
+                for (int i = 0; i < 8; i++)
+                {
+                    float bandVal = AudioPeer._audioBand[i];
 
-				if (double.IsNaN (bandVal))
-					bandVal = 0;
+                    if (double.IsNaN(bandVal))
+                        bandVal = 0;
 
-				if (bandVal >= max) {
-					max = bandVal;
-					maxIdx = i;
-				}
-//						float totalPercent = percentages [i];
-//						int middle = AudioPeer._avgFreqBand;
-//						float direction = (i - middle == 0 ? 0 : (i - middle > 0 ? 1 : -1));//((AudioPeer._audioBand [i] - AudioPeer._Amplitude) > 0 ? 1 : -1);
-//						rotVal += bandVal * totalPercent * direction;
-//						Debug.Log (i + " " + bandVal + " " + direction + " " + rotVal);
-			}
+                    if (bandVal >= max)
+                    {
+                        max = bandVal;
+                        maxIdx = i;
+                    }
+                    //						float totalPercent = percentages [i];
+                    //						int middle = AudioPeer._avgFreqBand;
+                    //						float direction = (i - middle == 0 ? 0 : (i - middle > 0 ? 1 : -1));//((AudioPeer._audioBand [i] - AudioPeer._Amplitude) > 0 ? 1 : -1);
+                    //						rotVal += bandVal * totalPercent * direction;
+                    //						Debug.Log (i + " " + bandVal + " " + direction + " " + rotVal);
+                }
 
-			float direction = (maxIdx - AudioPeer._avgFreqBand == 0 ? 0 : (maxIdx - AudioPeer._avgFreqBand > 0 ? 1 : -1));//((AudioPeer._audioBand [i] - AudioPeer._Amplitude) > 0 ? 1 : -1);
-			float nextVal = max * Mathf.Pow (2, Mathf.Abs (AudioPeer._avgFreqBand - maxIdx)) * direction;
-			rotVal = (rotVal + nextVal) / 2;
-			//rotVal /= 5;
-			lastIdx = maxIdx;
+                float direction = (maxIdx - AudioPeer._avgFreqBand == 0 ? 0 : (maxIdx - AudioPeer._avgFreqBand > 0 ? 1 : -1));//((AudioPeer._audioBand [i] - AudioPeer._Amplitude) > 0 ? 1 : -1);
+                float nextVal = max * Mathf.Pow(2, Mathf.Abs(AudioPeer._avgFreqBand - maxIdx)) * direction * (amtMoved * 2);
+                Debug.Log(nextVal);
+                rotVal = (rotVal + nextVal) / 2;
+                //rotVal /= 5;
+                lastIdx = maxIdx;
 
-//					if (rotVal > clampPos) {
-//						rotVal = clampPos;
-//						clampNeg += -.1f;
-//						biasPos = true;
-//						biasNeg = false;
-//					} else if (rotVal < clampNeg) {
-//						rotVal = clampNeg;
-//						biasPos = false;
-//						biasNeg = true;
-//					}
+                //					if (rotVal > clampPos) {
+                //						rotVal = clampPos;
+                //						clampNeg += -.1f;
+                //						biasPos = true;
+                //						biasNeg = false;
+                //					} else if (rotVal < clampNeg) {
+                //						rotVal = clampNeg;
+                //						biasPos = false;
+                //						biasNeg = true;
+                //					}
 
-//			if (biasPos) clampNeg += -.1f;
-//			else if (biasNeg) clampPos += .1f;
+                //			if (biasPos) clampNeg += -.1f;
+                //			else if (biasNeg) clampPos += .1f;
 
-			//clampNeg = (clampNeg * 9 + -1.0f) / 10f;
-			//clampPos = (clampPos * 9 + 1.0f) / 10f;
+                //clampNeg = (clampNeg * 9 + -1.0f) / 10f;
+                //clampPos = (clampPos * 9 + 1.0f) / 10f;
 
-			//Debug.Log (rotVal + " " + max + " " + maxIdx);
+                //Debug.Log (rotVal + " " + max + " " + maxIdx);
 
-			if (rotVal > 5)
-				rotVal = 5;
+                if (rotVal > 5)
+                    rotVal = 5;
 
-			angleXZ += rotVal;
+                //angleXZ += rotVal;
 
-			//Debug.Log (angleXZ);
+                //Debug.Log (angleXZ);
 
-			currRot = Quaternion.AngleAxis (angleXZ, Vector3.up);
-		}
-		if (frame % 5 == 0) {
-			float multiplier = car.GetComponent<Rigidbody> ().velocity.magnitude / 10;
-			angleY += .01f * multiplier;
-			currHeight = Mathf.Sin (angleY) * 10;
+                if (lr.positionCount > 10)
+                {
+                    angleXZ += rotVal;
+                    currRot = Quaternion.AngleAxis(angleXZ, Vector3.up);
+                }
 
-			Debug.Log (car.GetComponent<Rigidbody> ().velocity.magnitude);
-			currPoint += currRot * (Vector3.forward * multiplier);
-			currPoint.y = currHeight;
-			lr.positionCount += 1;
-			lr.SetPosition (lr.positionCount - 1, currPoint);
+            }
+            if (frame % 5 == 0)
+            {
+                float multiplier = car.GetComponent<Rigidbody>().velocity.magnitude / 10;
+                multiplier = amtMoved;
+                angleY += .01f * multiplier;
+                currHeight = Mathf.Sin(angleY) * 10;
 
-			const int posCount = 5;
-			if (lr.positionCount % posCount == 0) {
-				lr.GetPositions (trackPointArray);
-				int numPositions = lr.positionCount;
-				List<Vector3> l = new List<Vector3> ();
-				int forCount = (numPositions < posCount + 2 ? numPositions : posCount + 2);
-				for (int i = 0; i < forCount; i++)
-					l.Add (trackPointArray [numPositions - i - 1]);
-				Roadifier r = roadifier.GetComponent<Roadifier> ();
-				r.GenerateRoad (l);
-			}
-		}
+                //Debug.Log(amtMoved);
+                currPoint += currRot * (Vector3.forward * multiplier);
+                currPoint.y = currHeight;
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount - 1, currPoint);
 
-		frame++;
+                const int posCount = 1;
+                if (lr.positionCount % posCount == 0)
+                {
+                    lr.GetPositions(trackPointArray);
+                    int numPositions = lr.positionCount;
+                    List<Vector3> l = new List<Vector3>();
+                    int forCount = (numPositions < posCount + 2 ? numPositions : posCount + 2);
+                    for (int i = 0; i < forCount; i++)
+                        l.Add(trackPointArray[numPositions - i - 1]);
+                    Roadifier r = roadifier.GetComponent<Roadifier>();
+                    r.GenerateRoad(l);
+                }
+
+                lastCarPos = car.transform.position;
+            }
+
+            frame++;
+        }
 	}
 }
---
+++ Audio Visualization/Assets/Scripts/TriggerBasedOnFreqBand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBasedOnFreqBand : MonoBehaviour
+{
+    public GameObject debugVisualizer;
+
+    public int _band;
+    int frameNumber;
+    float lastBandVal = 0;
+
+    void Start()
+    { 
+        frameNumber = 0;
+    }
+
+    void Update()
+    {
+        float bandVal = AudioPeer._audioBand[_band];
+        if (bandVal - lastBandVal > .4)
+            Debug.Log(bandVal);
+
+        lastBandVal = bandVal;
+        frameNumber++;
+    }
+}
